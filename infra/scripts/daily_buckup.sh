72222325bdd56c4e723f015a56295cae311462ad
Fedi Houimli <104932358+Fadi5ann@users.noreply.github.com>
2026-05-01 22:48:30 +0100

    add(script): create daily backup script for automated database backups

---
+++ infra/scripts/daily_buckup.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+# ZC-Invoice Automated Daily Backup
+
+# 1. Get today's date (e.g., 2026-05-01)
+DATE=$(date +%Y-%m-%d)
+
+# 2. Run the backup (Notice we removed '-it' because Cron doesn't have an interactive terminal!)
+docker exec zc-backup-v200 bash -c "mysqldump -h 10.0.3.12 -u root -prootpassword --all-databases --single-transaction --routines --triggers --events --set-gtid-purged=OFF > /backup-vault/backup-$DATE.sql"
+
+# 3. Housekeeping: Delete backups older than 7 days
+find /home/fadi5an/zc-invoice-project/backups -type f -name "*.sql" -mtime +7 -delete
