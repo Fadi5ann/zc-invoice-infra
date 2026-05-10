05d88fefe93bffdb7fae967bf1fe30b6e9aff9fa
Fedi Houimli <104932358+Fadi5ann@users.noreply.github.com>
2026-05-10 15:28:54 +0100

    fix: update import statement and correct refetchInterlocutors function calls in InterlocutorCreateOrAssociateSheet; modify form structure in useCreateRefParamFormStructure for title and description

--- infra/frontend/src/components/contacts/interlocutor/modals/InterlocutorCreateOrAssociateSheet.tsx
+++ infra/frontend/src/components/contacts/interlocutor/modals/InterlocutorCreateOrAssociateSheet.tsx
@@ -6,7 +6,7 @@ import { Spinner } from '@/components/shared';
 import { Tabs, TabsContent, TabsList, TabsTrigger } from '@/components/ui/tabs';
 import { InterlocutorContactInformation } from '../form/InterlocutorContactInformation';
 import { InterlocutorAssociation } from '../form/InterlocutorAssociation';
-import useInterlocutors from '@/hooks/content/core/useEnterpriseInterlocutors';
+import { useEnterpriseInterlocutors } from '@/hooks/content/core/useEnterpriseInterlocutors';
 import React from 'react';
 import { firm } from '@/api';
 
@@ -26,11 +26,11 @@ export const useInterlocutorCreateOrAssociateSheet = (
     resetInterlocutor?.();
   };
 
-  const { interlocutors, isFetchInterlocutorsPending, refetchInterloctors } = useInterlocutors();
+  const { interlocutors, isFetchInterlocutorsPending, refetchInterlocutors } = useEnterpriseInterlocutors({ enterpriseId: firmId });
 
   React.useEffect(() => {
-    if (firmId) refetchInterloctors();
-  }, [firmId, refetchInterloctors]);
+    if (firmId) refetchInterlocutors();
+  }, [firmId, refetchInterlocutors]);
 
   const {
     SheetFragment: createInterlocutorSheet,
@@ -72,7 +72,7 @@ export const useInterlocutorCreateOrAssociateSheet = (
               if (activeTab === 'new') createInterlocutor?.();
               else {
                 associateInterlocutor?.();
-                refetchInterloctors();
+                refetchInterlocutors();
               }
             }}>
             {tCommon('commands.save')}
