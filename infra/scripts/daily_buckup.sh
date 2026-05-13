662e97a5fcbb101891a0037cb205248264014467
Fedi Houimli <104932358+Fadi5ann@users.noreply.github.com>
2026-05-13 10:50:09 +0100

    #comment

--- infra/scripts/daily_buckup.sh
+++ infra/scripts/daily_buckup.sh
@@ -4,7 +4,7 @@
 # 1. Get today's date (e.g., 2026-05-01)
 DATE=$(date +%Y-%m-%d)
 
-# 2. Run the backup (Notice we removed '-it' because Cron doesn't have an interactive terminal!)
+# 2. Run the backup 
 docker exec zc-backup-v200 bash -c "mysqldump -h 10.0.3.12 -u root -prootpassword --all-databases --single-transaction --routines --triggers --events --set-gtid-purged=OFF > /backup-vault/backup-$DATE.sql"
 
 # 3. Housekeeping: Delete backups older than 7 days
