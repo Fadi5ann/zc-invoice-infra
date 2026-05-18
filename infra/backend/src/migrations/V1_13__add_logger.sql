eb5c3b84409183d53fc02fa98b0b24f3d019b2ba
Fedi Houimli <104932358+Fadi5ann@users.noreply.github.com>
2026-05-18 17:29:58 +0100

    fix: adjust logger table definition and improve Nginx routing for API calls

--- infra/backend/src/migrations/V1_13__add_logger.sql
+++ infra/backend/src/migrations/V1_13__add_logger.sql
@@ -1,13 +1,12 @@
 CREATE TABLE
     `logger` (
-        `id` INT NOT NULL AUTO_INCREMENT,
+        `id` INT AUTO_INCREMENT PRIMARY KEY,
         `event` VARCHAR(255) NULL,
         `api` VARCHAR(255) NULL,
         `method` VARCHAR(50) NULL,
         `userId` VARCHAR(255) NULL,
         `logInfo` json DEFAULT NULL,
         `loggedAt` TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
-        PRIMARY KEY (`id`),
         KEY `FK_user_logger` (`userId`),
         CONSTRAINT `FK_user_logger` FOREIGN KEY (`userId`) REFERENCES `user` (`id`) ON DELETE SET NULL
     );
